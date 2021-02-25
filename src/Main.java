1e361540e070c46a150ceb4a62acfba9675709c3
ptwestersautos@gmail.com <1937italo7319>
2021-02-25 08:52:10 +0100

    task7-8

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ public class Main {
 	// write your code here
         //System.out.println(NewtonMethod.solve(1, 0.001, 100));
        //
-        System.out.println(SecantMethod.solve(0.5, 1.5, 0.001, 100));
+
+        //System.out.println(SecantMethod.solve(0.5, 1.5, 0.001, 100));
         // System.out.println(BisectionMethod.solve(-0.5,1.5,0.001, 100));
+
+
+        System.out.println(0.1+0.2);
+        System.out.println(0.1+0);
+        System.out.println(0.1+0.1);
+
+
     }
 }
